367a0d4ccf52afb4629c2e22574fa0446bdf8821
Ayoub Boudouaya <boudouayou@gmail.com>
2024-02-24 12:50:23 +0100

    QOL : Disables navigation while in MosaicMode.

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -27,18 +27,19 @@ function Deck({children}) {
         }
     }, [currentSlide, totalSlides]);
 
-    // Toggle mosaic view
-    const toggleMosaicView = () => {
-        setMosaicView(!mosaicView);
+    // GoToSlide
+    const goToSlide = (index) => {
+        setCurrentSlide(index);
+        setMosaicView(false);
     };
 
     // Keyboard navigation
     useEffect(() => {
         const handleKeyUp = (event) => {
-            if (event.key === "ArrowRight") {
+            if (event.key === "ArrowRight" && !mosaicView) {
                 nextSlide();
             }
-            if (event.key === "ArrowLeft") {
+            if (event.key === "ArrowLeft" && !mosaicView) {
                 prevSlide();
             }
         };
@@ -58,31 +59,39 @@ function Deck({children}) {
     return (
         <div className={"deck"}>
             {mosaicView ?
-                <MosaicView slides={slides}/>
+                <MosaicView slides={slides} goToSlideFromMosaic={goToSlide}/>
                 : <div className="slide h-screen w-screen text-center flex flex-col justify-center
                     bg-background dark:bg-stone-800 overflow-clip"> {slides[currentSlide]}</div>
             }
             <div className="toolbar fixed bottom-0 left-0 flex flex-row justify-start px-4 w-full text-accent
-             dark:text-primary">
-                <button className="prev" onClick={prevSlide}><span
+            dark:text-primary">
+                <button disabled={mosaicView} className="prev disabled:text-gray-600" onClick={prevSlide}><span
                     className="material-symbols-outlined">arrow_back_ios</span></button>
-                <button className="next" onClick={nextSlide}><span
+                <button disabled={mosaicView} className="next disabled:text-gray-600" onClick={nextSlide}><span
                     className="material-symbols-outlined">arrow_forward_ios</span></button>
-                <button className="mosaic-view" onClick={toggleMosaicView}><span
+                <button className="mosaic-view" onClick={() => setMosaicView(!mosaicView)}><span
                     className="material-symbols-outlined">view_cozy</span></button>
             </div>
-            <div className="progress-bar fixed top-0 left-0 w-0 h-2 transition-all duration-500 ease-in-out bg-primary dark:bg-primary" style={{width: `${progressWidth}%`}}/>
+            {!mosaicView ?
+            <div
+                className="progress-bar fixed top-0 left-0 w-0 h-2 transition-all duration-500 ease-in-out
+                bg-primary dark:bg-primary"
+                style={{width: `${progressWidth}%`}}/>:""
+            }
         </div>
     );
 }
 
+// TODO : Verify that a big image doesn't cause lag on mosaic view.
+
 // Mosaic view
-function MosaicView({slides}) {
+function MosaicView({slides, goToSlideFromMosaic}) {
     return (
         <div className={"mosaic-view grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-4"}>
             {slides.map((slide, index) => (
                 <div key={index} className="mosaic-slide h-96 w-full rounded-lg flex flex-col
-                    justify-center bg-background dark:bg-stone-800 items-center overflow-clip">
+                    justify-center bg-background dark:bg-stone-800 items-center overflow-clip cursor-pointer"
+                    onClick={() => goToSlideFromMosaic(index)}>
                     <div className="mosaic-slide-content scale-[0.4] flex flex-col items-center text-center mx-0">
                         {slide}
                     </div>
@@ -94,7 +103,8 @@ function MosaicView({slides}) {
 
 // Prop types
 MosaicView.propTypes = {
-    slides: PropTypes.arrayOf(PropTypes.node).isRequired
+    slides: PropTypes.arrayOf(PropTypes.node).isRequired,
+    goToSlideFromMosaic: PropTypes.func.isRequired
 };
 
 Deck.propTypes = {
